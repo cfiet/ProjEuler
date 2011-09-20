51f2a4aa0a414aa73827fd53b09dc4381f3c7640
cfiet <cfiet@cfiet.net>
2011-09-20 14:03:06 +0200

    Solution to problem #10 drastically improved. Run around 1.5 sec when interpreted, around 0,3 when compied with -O

--- e10.hs
+++ e10.hs
@@ -1,30 +1,40 @@
 import Data.Array.IO
 
 limitSize = 2000000
+arraySize = (limitSize `div` 2) - 1
+sieveLimit = ceiling $ sqrt $ fromInteger arraySize
 --sieveLimit = ceiling $ sqrt $ fromInteger limitSize
 
+indexToNumber i =
+	3 + 2*i
+
+numberToIndex n =
+	(n-3) `div` 2
 
 sieveRemove arr s i
-	| i+s <= limitSize = do 
-				writeArray arr (i+s) False 
-				sieveRemove arr s (i+s)
+	| i <= arraySize = do
+		writeArray arr i False
+		sieveRemove arr s (i+s)
 	| otherwise = return ()
 
 sieve arr i
-	| i <= limitSize = do
-					v <- readArray arr i
-					case v of
-						True -> sieveRemove arr i i
-						_ -> return ()
-					sieve arr (i+1)
+	| i <= numberToIndex sieveLimit =
+		let n = indexToNumber i in do
+			v <- readArray arr i
+			case v of
+				True ->sieveRemove arr n (numberToIndex $ n*n)
+				_ -> return ()
+			sieve arr (i+1)
 	| otherwise = return ()
 
-
+sumA arr i s
+	| i <= arraySize = do
+		v <- readArray arr i
+		let ns = case v of {True -> (s+3+2*i); False -> s;} in sumA arr (i+1) ns
+	| otherwise = return s
 
 main = do
-	arr <- newArray (2, limitSize) $ True :: IO (IOUArray Integer Bool)
-	sieve arr 2
-	e <- getElems arr
-	let s = map (\(x,y) -> x) $ filter (\(x,y) -> y == True) $ zip [2..] e in 
-		putStrLn $ show $ sum s
-	return ()
+	arr <- newArray (0, arraySize) $ True :: IO (IOUArray Integer Bool)
+	sieve arr 0
+	s <- sumA arr 0 0
+	putStrLn . show $ s
