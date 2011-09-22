6dd85543d47aa1de77aa1bfb98db02c6de4ca864
cfiet <cfiet@cfiet.net>
2011-09-22 14:03:55 +0200

    Solution for problem #14 - runs about 5 sec but uses at least 4kb of memory.

--- e14.hs
+++ e14.hs
@@ -2,10 +2,9 @@ import Data.Array.IO
 
 searchLimit = 1000000
 
-seq n
-	| n `mod` 2 == 0 = n / 2
+calcSeq n
+	| n `mod` 2 == 0 = n `div` 2
 	| otherwise = 3 * n + 1
-
 
 replaceNum arr nnum ndist nparent =
 	do
@@ -13,25 +12,31 @@ replaceNum arr nnum ndist nparent =
 		writeArray arr (nnum,2) ndist
 		return ()
 
+calculateDistance :: IOUArray Int Int -> Integer -> IO (Int)
+calculateDistance arr num =
+	do
+		n <- if num <= searchLimit then readArray arr (fromInteger num) else return (-1)
+		distance <- case n of
+						-1 -> do
+							d <- calculateDistance arr $ calcSeq num
+							return (d+1)
+						otherwise -> return n
+		if num <= searchLimit then writeArray arr (fromInteger num) distance else return ()
+		return distance
 
-
-
-
-checkNum arr num dist =
-	let nnum = seq n in
-		if nnum == 1 then return ()
-		else do
-			odist <- readArray (nnum,2)
-			if odist == -1 then
-			else
-				if odist < dist then replaceNum arr nnum (dist+1)
-
-
-
+getMaxDistance :: IOUArray Int Int -> (Integer,Int) -> [Integer] -> IO (Integer,Int)
+getMaxDistance arr max [] = return (max)
+getMaxDistance arr (maxIndex, maxValue) (a:x) = do
+	d <- calculateDistance arr a
+	let newMax = if d > maxValue then (a,d) else (maxIndex, maxValue)
+		in getMaxDistance arr newMax x
+		
 main = do
-	p <- newArray ((1,1),(searchLimit, 2)) (-1) :: IO (IOUArray (Int,Int) Int)
-	writeArray p (1,1) 0
-	writeArray p (1,2) 1
-	return ()
+	p <- newArray (1, 1000000) (-1) :: IO (IOUArray Int Int)
+	writeArray p 1 1
+	d <- getMaxDistance p (0,0) [999999, 999997..500000]
+	putStrLn $ show d
+	c <- getElems p
+	putStrLn . show . length $ filter (==(-1)) c
 
 
