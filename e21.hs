58503c140d0df7ecbb5625fe16b729f37f743f34
cfiet <cfiet@cfiet.net>
2011-09-30 09:51:49 +0200

    Solved problem #21.

---
+++ e21.hs
@@ -0,0 +1,41 @@
+import qualified Data.List as L
+import qualified Data.Map as M
+
+foldDivisors :: M.Map Integer [Integer] -> [Integer] -> Integer -> [Integer]
+foldDivisors m [] a = []
+foldDivisors m (k:ks) a
+    | reminder == 0 = let dk = M.findWithDefault [] k m 
+                          h = [k, (a `div` k)]
+                          in h ++ dk ++ (foldDivisors m ks a)
+    | otherwise = (foldDivisors m ks a)
+    where reminder = a `mod` k
+
+getDivisors :: M.Map Integer [Integer] -> Integer -> M.Map Integer [Integer]
+getDivisors m a =
+    let v = [k | k <- M.keys m, k > 1, k <= (floor . sqrt . fromInteger $ a)]
+        d = L.nub $ (1 : foldDivisors m v a)
+        in M.insert a d m
+ 
+generateDivisors m [] = m
+generateDivisors m (x:xs) =
+    let n = getDivisors m x
+        in generateDivisors n xs
+
+findAmicable :: M.Map Integer [Integer] -> [Integer]
+findAmicable m
+    | m == M.empty = []
+    | otherwise = let nn = sum d
+                      nd = M.findWithDefault [] nn m
+                      rst = M.delete n m
+                      (e, mm) = if n /= nn && (sum nd == n) then
+                                    ([n, nn], M.delete nn rst)
+                                else ([], rst)
+                      h = if n == nn then [] else e
+                      in (h ++ findAmicable mm)
+                         
+    where ((n,d):xs) = M.toList m
+
+main = do
+    let im = M.fromList [(1, [1]), (2, [1]), (3,[1])] 
+        d = sum $ findAmicable $ generateDivisors im [4..10000]
+        in putStrLn $ show d
